44f169eda141129401733ea4d41dd562cc31402e
Axl Aparicio <axelaparicior@gmail.com>
2024-05-16 12:18:48 -0300

    Added more functionality to dropoff and locate

--- lib/cars_app_web/controllers/page_controller.ex
+++ lib/cars_app_web/controllers/page_controller.ex
@@ -19,23 +19,46 @@ defmodule CarsAppWeb.PageController do
     send_resp(conn, 200, "")
   end
 
-  def dropoff(conn, _params) do
-    send_resp(conn, 200, "")
+  def dropoff(conn, %{"ID" => id}) do
+    case Redix.command(:redix, ["GET", id]) do
+      {:error, _} -> send_resp(conn, 500, "500 Redis connection closed")
+      {:ok, nil} -> send_resp(conn, 404, "404 Not Found")
+      {:ok, _group_id} ->
+        # TODO: Send the correct response
+        # 200 OK or 204 No Content When the group is unregistered correctly
+        send_resp(conn, 200, "200 OK")
+    end
   end
 
-  def locate(conn, _params) do
-    send_resp(conn, 200, "")
+  def dropoff(conn, _) do
+    send_resp(conn, 400, "400 Bad Request")
+  end
+
+  def locate(conn, %{"ID" => id}) do
+    case Redix.command(:redix, ["GET", id]) do
+      {:error, _} -> send_resp(conn, 500, "500 Redis connection closed")
+      {:ok, nil} -> send_resp(conn, 404, "404 Not Found")
+      {:ok, _group_id} ->
+        # TODO: Send the correct response
+        # * 200 OK With the car as the payload when the group is assigned to a car.
+        # * 204 No Content When the group is waiting to be assigned to a car.
+        send_resp(conn, 200, "200 OK")
+    end
+  end
+
+  def locate(conn, _) do
+    send_resp(conn, 400, "400 Bad Request")
   end
 
   defp send_response_by_redis_status(conn, {:error, %Redix.ConnectionError{reason: :closed}}) do
-    send_resp(conn, 500, "Redis connection closed")
+    send_resp(conn, 500, "500 Redis connection closed")
   end
 
   defp send_response_by_redis_status(conn, {:error, _}) do
-    send_resp(conn, 520, "Unknown error")
+    send_resp(conn, 520, "520 Unknown error")
   end
 
   defp send_response_by_redis_status(conn, {:ok, _}) do
-    send_resp(conn, 200, "OK")
+    send_resp(conn, 200, "200 OK")
   end
 end
