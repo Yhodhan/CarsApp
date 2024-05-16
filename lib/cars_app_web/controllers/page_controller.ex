b48af56f891b786974cc32519063e11898cc2f7d
Axl Aparicio <axelaparicior@gmail.com>
2024-05-15 23:55:54 -0300

    Change status code

--- lib/cars_app_web/controllers/page_controller.ex
+++ lib/cars_app_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule CarsAppWeb.PageController do
   use CarsAppWeb, :controller
 
   def home(conn, _params) do
-    send_resp(conn, 201, "Welcome to Cars App")
+    send_resp(conn, 200, "Welcome to Cars App")
   end
 end
